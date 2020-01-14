c6f67e6c5fdb374d4440ba11f509fe47911ca017
Daniel Bozinovski <BozinovskiDaniel@hotmail.com>
2020-01-14 15:08:40 +1100

    added more to house scraper

--- House_Scraper.py
+++ House_Scraper.py
@@ -10,32 +10,56 @@ import seaborn as sns
 sns.set()
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
 
-url = 'https://www.domain.com.au/sale/wetherill-park-nsw-2164/?excludeunderoffer=1'
-
-response  = requests.get(url, headers=headers)
-
-if response.ok:
+# Gets the property info on the given page
+def getPropertyInfo(pageNum):
+    url = 'https://www.domain.com.au/sale/wetherill-park-nsw-2164/?excludeunderoffer=1&page=' + str(pageNum)
+    response  = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     houses = soup.findAll("li", {"class": "css-1b4kfhp"})
-    prices = []
-    locations = []
 
+    for house in houses:
+        price = house.find('p', {'class': 'listing-result__price'})
+        location = house.find('span', {'class': 'address-line1'})
+        property_type = house.find('div', {'class': 'listing-result__property-type'})
+        link = house.find_all('a')
 
-    for i in range(0, 20):
-        price = houses[i].find('p', {'class': 'listing-result__price'})
-        location = houses[i].find('span', {'class': 'address-line1'})
-
-
-        if price == None or location ==  None:
+        if price == None or location ==  None or property_type == None or (price.text)[0] != '$':
             continue
         else:
             prices.append(price.text)
             newLocation = (location.text).replace(',\xa0', '')
             locations.append(newLocation)
-
-    print(locations)
-    print(prices)
-    
+            property_types.append(property_type.text)
+            links.append(link)
 
 
+prices = []
+locations = []
+property_types = []
+links = []
+
+for i in range(1, 20):
+    getPropertyInfo(i)
+
+print(property_types)
+#print(prices) 
+#print(locations)
+
+
+
+
+# Methods
+def fixPrice(price):
+
+    if price[0] == '$':
+        newPrice = ((price.replace('$', '')).replace(',', '')).replace(' ', '')
+
+
+    return int(newPrice)
+
+# Testing
+def test_one():
+
+    assert fixPrice('$665,000 ') == 665000
+    assert fixPrice()
